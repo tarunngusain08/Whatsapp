c124399d003918ad9ff93de772ca3e50c7b509e2
Tarun Gusain <Tarun.Gusain@gruve.ai@Taruns-MacBook-Pro.local>
2026-02-16 11:28:15 +0530

    vibe coded the client and all the backend services: AuthDtos.kt

---
+++ client/core/network/src/main/java/com/whatsappclone/core/network/model/dto/AuthDtos.kt
@@ -0,0 +1,41 @@
+package com.whatsappclone.core.network.model.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SendOtpRequest(
+    val phone: String
+)
+
+@Serializable
+data class SendOtpResponse(
+    val message: String,
+    @SerialName("expires_in_seconds")
+    val expiresInSeconds: Int
+)
+
+@Serializable
+data class VerifyOtpRequest(
+    val phone: String,
+    val otp: String
+)
+
+@Serializable
+data class AuthTokenResponse(
+    @SerialName("access_token")
+    val accessToken: String,
+    @SerialName("refresh_token")
+    val refreshToken: String,
+    @SerialName("expires_in_seconds")
+    val expiresInSeconds: Int,
+    val user: UserDto,
+    @SerialName("is_new_user")
+    val isNewUser: Boolean = false
+)
+
+@Serializable
+data class RefreshRequest(
+    @SerialName("refresh_token")
+    val refreshToken: String
+)
