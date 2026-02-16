24fe74d10a81811ebf91bf715b185fd9d10ba021
Tarun Gusain <Tarun.Gusain@gruve.ai@Taruns-MacBook-Pro.local>
2026-02-16 11:29:14 +0530

    vibe coded the client and all the backend services: ProfileSetupViewModel_HiltModules.java

---
+++ client/feature/auth/build/kspCaches/debug/backups/java/byRounds/1/com/whatsappclone/feature/auth/ui/profile/ProfileSetupViewModel_HiltModules.java
@@ -0,0 +1,51 @@
+package com.whatsappclone.feature.auth.ui.profile;
+
+import androidx.lifecycle.ViewModel;
+import dagger.Binds;
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.ActivityRetainedComponent;
+import dagger.hilt.android.components.ViewModelComponent;
+import dagger.hilt.android.internal.lifecycle.HiltViewModelMap;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.multibindings.IntoMap;
+import dagger.multibindings.LazyClassKey;
+import javax.annotation.processing.Generated;
+
+@Generated("dagger.hilt.android.processor.internal.viewmodel.ViewModelProcessor")
+@OriginatingElement(
+    topLevelClass = ProfileSetupViewModel.class
+)
+public final class ProfileSetupViewModel_HiltModules {
+  private ProfileSetupViewModel_HiltModules() {
+  }
+
+  @Module
+  @InstallIn(ViewModelComponent.class)
+  public abstract static class BindsModule {
+    private BindsModule() {
+    }
+
+    @Binds
+    @IntoMap
+    @LazyClassKey(ProfileSetupViewModel.class)
+    @HiltViewModelMap
+    public abstract ViewModel binds(ProfileSetupViewModel vm);
+  }
+
+  @Module
+  @InstallIn(ActivityRetainedComponent.class)
+  public static final class KeyModule {
+    private KeyModule() {
+    }
+
+    @Provides
+    @IntoMap
+    @LazyClassKey(ProfileSetupViewModel.class)
+    @HiltViewModelMap.KeySet
+    public static boolean provide() {
+      return true;
+    }
+  }
+}
