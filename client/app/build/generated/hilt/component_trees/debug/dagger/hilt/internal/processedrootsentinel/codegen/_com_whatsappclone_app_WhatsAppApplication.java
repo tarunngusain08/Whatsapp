1a785bedeb7b33665fcbc3f0b8b43854a5a4531b
Tarun Gusain <Tarun.Gusain@gruve.ai@Taruns-MacBook-Pro.local>
2026-02-16 11:24:43 +0530

    vibe coded the client and all the backend services: _com_whatsappclone_app_WhatsAppApplication.java

---
+++ client/app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_whatsappclone_app_WhatsAppApplication.java
@@ -0,0 +1,9 @@
+package dagger.hilt.internal.processedrootsentinel.codegen;
+
+import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
+
+@ProcessedRootSentinel(
+    roots = "com.whatsappclone.app.WhatsAppApplication"
+)
+public final class _com_whatsappclone_app_WhatsAppApplication {
+}
