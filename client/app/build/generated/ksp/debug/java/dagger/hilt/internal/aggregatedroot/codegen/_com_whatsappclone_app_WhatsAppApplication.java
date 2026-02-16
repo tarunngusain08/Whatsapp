bda3f98ae38611f4c0c1e3f071deff28668ac1c2
Tarun Gusain <Tarun.Gusain@gruve.ai@Taruns-MacBook-Pro.local>
2026-02-16 11:24:44 +0530

    vibe coded the client and all the backend services: _com_whatsappclone_app_WhatsAppApplication.java

---
+++ client/app/build/generated/ksp/debug/java/dagger/hilt/internal/aggregatedroot/codegen/_com_whatsappclone_app_WhatsAppApplication.java
@@ -0,0 +1,21 @@
+package dagger.hilt.internal.aggregatedroot.codegen;
+
+import dagger.hilt.android.HiltAndroidApp;
+import dagger.hilt.internal.aggregatedroot.AggregatedRoot;
+import javax.annotation.processing.Generated;
+
+/**
+ * This class should only be referenced by generated code! This class aggregates information across multiple compilations.
+ */
+@AggregatedRoot(
+    root = "com.whatsappclone.app.WhatsAppApplication",
+    rootPackage = "com.whatsappclone.app",
+    originatingRoot = "com.whatsappclone.app.WhatsAppApplication",
+    originatingRootPackage = "com.whatsappclone.app",
+    rootAnnotation = HiltAndroidApp.class,
+    rootSimpleNames = "WhatsAppApplication",
+    originatingRootSimpleNames = "WhatsAppApplication"
+)
+@Generated("dagger.hilt.processor.internal.root.AggregatedRootGenerator")
+public class _com_whatsappclone_app_WhatsAppApplication {
+}
