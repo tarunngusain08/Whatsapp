179ea9fd4b51910812fd199531f3fcb7dec30249
Tarun Gusain <Tarun.Gusain@gruve.ai@Taruns-MacBook-Pro.local>
2026-02-16 11:35:18 +0530

    vibe coded the client and all the backend services: PrivacySettingsViewModel_HiltModules_KeyModule_Provide_LazyMapKey.java

---
+++ client/feature/settings/build/generated/ksp/debug/java/com/whatsappclone/feature/settings/ui/PrivacySettingsViewModel_HiltModules_KeyModule_Provide_LazyMapKey.java
@@ -0,0 +1,28 @@
+package com.whatsappclone.feature.settings.ui;
+
+import dagger.internal.DaggerGenerated;
+import dagger.internal.IdentifierNameString;
+import dagger.internal.KeepFieldType;
+import javax.annotation.processing.Generated;
+
+@IdentifierNameString
+@DaggerGenerated
+@Generated(
+    value = "dagger.internal.codegen.ComponentProcessor",
+    comments = "https://dagger.dev"
+)
+@SuppressWarnings({
+    "unchecked",
+    "rawtypes",
+    "KotlinInternal",
+    "KotlinInternalInJava",
+    "cast",
+    "deprecation",
+    "nullness:initialization.field.uninitialized"
+})
+public final class PrivacySettingsViewModel_HiltModules_KeyModule_Provide_LazyMapKey {
+  @KeepFieldType
+  static PrivacySettingsViewModel keepFieldType;
+
+  public static String lazyClassKeyName = "com.whatsappclone.feature.settings.ui.PrivacySettingsViewModel";
+}
