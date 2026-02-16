33114bfdbcd8a98806f039d109728e5cf85c0633
Tarun Gusain <Tarun.Gusain@gruve.ai@Taruns-MacBook-Pro.local>
2026-02-16 11:33:51 +0530

    vibe coded the client and all the backend services: SharedMediaViewModel_HiltModules_KeyModule_Provide_LazyMapKey.java

---
+++ client/feature/media/build/generated/ksp/debug/java/com/whatsappclone/feature/media/ui/SharedMediaViewModel_HiltModules_KeyModule_Provide_LazyMapKey.java
@@ -0,0 +1,28 @@
+package com.whatsappclone.feature.media.ui;
+
+import dagger.internal.DaggerGenerated;
+import dagger.internal.IdentifierNameString;
+import dagger.internal.KeepFieldType;
+import javax.annotation.processing.Generated;
+
+@IdentifierNameString
+@DaggerGenerated
+@Generated(
+    value = "dagger.internal.codegen.ComponentProcessor",
+    comments = "https://dagger.dev"
+)
+@SuppressWarnings({
+    "unchecked",
+    "rawtypes",
+    "KotlinInternal",
+    "KotlinInternalInJava",
+    "cast",
+    "deprecation",
+    "nullness:initialization.field.uninitialized"
+})
+public final class SharedMediaViewModel_HiltModules_KeyModule_Provide_LazyMapKey {
+  @KeepFieldType
+  static SharedMediaViewModel keepFieldType;
+
+  public static String lazyClassKeyName = "com.whatsappclone.feature.media.ui.SharedMediaViewModel";
+}
