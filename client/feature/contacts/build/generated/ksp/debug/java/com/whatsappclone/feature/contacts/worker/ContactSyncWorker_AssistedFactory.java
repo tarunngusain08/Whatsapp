628fb8b734e04d6525b7c0cc58f46fafbf21163d
Tarun Gusain <Tarun.Gusain@gruve.ai@Taruns-MacBook-Pro.local>
2026-02-16 11:31:50 +0530

    vibe coded the client and all the backend services: ContactSyncWorker_AssistedFactory.java

---
+++ client/feature/contacts/build/generated/ksp/debug/java/com/whatsappclone/feature/contacts/worker/ContactSyncWorker_AssistedFactory.java
@@ -0,0 +1,10 @@
+package com.whatsappclone.feature.contacts.worker;
+
+import androidx.hilt.work.WorkerAssistedFactory;
+import dagger.assisted.AssistedFactory;
+import javax.annotation.processing.Generated;
+
+@Generated("androidx.hilt.AndroidXHiltProcessor")
+@AssistedFactory
+public interface ContactSyncWorker_AssistedFactory extends WorkerAssistedFactory<ContactSyncWorker> {
+}
