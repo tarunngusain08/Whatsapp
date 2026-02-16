79696cf980089145a01fdeaae384e69f8c1acca2
Tarun Gusain <Tarun.Gusain@gruve.ai@Taruns-MacBook-Pro.local>
2026-02-16 11:30:04 +0530

    vibe coded the client and all the backend services: GlobalSearchViewModel_HiltModules.java

---
+++ client/feature/chat/build/generated/ksp/debug/java/com/whatsappclone/feature/chat/ui/search/GlobalSearchViewModel_HiltModules.java
@@ -0,0 +1,51 @@
+package com.whatsappclone.feature.chat.ui.search;
+
+import androidx.lifecycle.ViewModel;
+import dagger.Binds;
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.ActivityRetainedComponent;
+import dagger.hilt.android.components.ViewModelComponent;
+import dagger.hilt.android.internal.lifecycle.HiltViewModelMap;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.multibindings.IntoMap;
+import dagger.multibindings.LazyClassKey;
+import javax.annotation.processing.Generated;
+
+@Generated("dagger.hilt.android.processor.internal.viewmodel.ViewModelProcessor")
+@OriginatingElement(
+    topLevelClass = GlobalSearchViewModel.class
+)
+public final class GlobalSearchViewModel_HiltModules {
+  private GlobalSearchViewModel_HiltModules() {
+  }
+
+  @Module
+  @InstallIn(ViewModelComponent.class)
+  public abstract static class BindsModule {
+    private BindsModule() {
+    }
+
+    @Binds
+    @IntoMap
+    @LazyClassKey(GlobalSearchViewModel.class)
+    @HiltViewModelMap
+    public abstract ViewModel binds(GlobalSearchViewModel vm);
+  }
+
+  @Module
+  @InstallIn(ActivityRetainedComponent.class)
+  public static final class KeyModule {
+    private KeyModule() {
+    }
+
+    @Provides
+    @IntoMap
+    @LazyClassKey(GlobalSearchViewModel.class)
+    @HiltViewModelMap.KeySet
+    public static boolean provide() {
+      return true;
+    }
+  }
+}
