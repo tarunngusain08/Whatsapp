3313a413f9f28730157b12450049cf7ea27a46d7
Tarun Gusain <Tarun.Gusain@gruve.ai@Taruns-MacBook-Pro.local>
2026-02-16 11:33:51 +0530

    vibe coded the client and all the backend services: MediaCleanupWorker_HiltModule.java

---
+++ client/feature/media/build/generated/ksp/debug/java/com/whatsappclone/feature/media/worker/MediaCleanupWorker_HiltModule.java
@@ -0,0 +1,26 @@
+package com.whatsappclone.feature.media.worker;
+
+import androidx.hilt.work.WorkerAssistedFactory;
+import androidx.work.ListenableWorker;
+import dagger.Binds;
+import dagger.Module;
+import dagger.hilt.InstallIn;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.hilt.components.SingletonComponent;
+import dagger.multibindings.IntoMap;
+import dagger.multibindings.StringKey;
+import javax.annotation.processing.Generated;
+
+@Generated("androidx.hilt.AndroidXHiltProcessor")
+@Module
+@InstallIn(SingletonComponent.class)
+@OriginatingElement(
+    topLevelClass = MediaCleanupWorker.class
+)
+public interface MediaCleanupWorker_HiltModule {
+  @Binds
+  @IntoMap
+  @StringKey("com.whatsappclone.feature.media.worker.MediaCleanupWorker")
+  WorkerAssistedFactory<? extends ListenableWorker> bind(
+      MediaCleanupWorker_AssistedFactory factory);
+}
