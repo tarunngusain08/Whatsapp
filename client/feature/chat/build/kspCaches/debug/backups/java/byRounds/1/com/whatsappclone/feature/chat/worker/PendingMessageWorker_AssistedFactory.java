df7a30910aa24fcf5d8a88265eaae1c2501a21c2
Tarun Gusain <Tarun.Gusain@gruve.ai@Taruns-MacBook-Pro.local>
2026-02-16 11:31:08 +0530

    vibe coded the client and all the backend services: PendingMessageWorker_AssistedFactory.java

---
+++ client/feature/chat/build/kspCaches/debug/backups/java/byRounds/1/com/whatsappclone/feature/chat/worker/PendingMessageWorker_AssistedFactory.java
@@ -0,0 +1,10 @@
+package com.whatsappclone.feature.chat.worker;
+
+import androidx.hilt.work.WorkerAssistedFactory;
+import dagger.assisted.AssistedFactory;
+import javax.annotation.processing.Generated;
+
+@Generated("androidx.hilt.AndroidXHiltProcessor")
+@AssistedFactory
+public interface PendingMessageWorker_AssistedFactory extends WorkerAssistedFactory<PendingMessageWorker> {
+}
