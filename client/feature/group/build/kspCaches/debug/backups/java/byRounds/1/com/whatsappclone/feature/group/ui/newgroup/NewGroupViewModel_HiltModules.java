8b4ca559b55b458338641075adeb34211e275b3d
Tarun Gusain <Tarun.Gusain@gruve.ai@Taruns-MacBook-Pro.local>
2026-02-16 11:33:23 +0530

    vibe coded the client and all the backend services: NewGroupViewModel_HiltModules.java

---
+++ client/feature/group/build/kspCaches/debug/backups/java/byRounds/1/com/whatsappclone/feature/group/ui/newgroup/NewGroupViewModel_HiltModules.java
@@ -0,0 +1,51 @@
+package com.whatsappclone.feature.group.ui.newgroup;
+
+import androidx.lifecycle.ViewModel;
+import dagger.Binds;
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.ActivityRetainedComponent;
+import dagger.hilt.android.components.ViewModelComponent;
+import dagger.hilt.android.internal.lifecycle.HiltViewModelMap;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.multibindings.IntoMap;
+import dagger.multibindings.LazyClassKey;
+import javax.annotation.processing.Generated;
+
+@Generated("dagger.hilt.android.processor.internal.viewmodel.ViewModelProcessor")
+@OriginatingElement(
+    topLevelClass = NewGroupViewModel.class
+)
+public final class NewGroupViewModel_HiltModules {
+  private NewGroupViewModel_HiltModules() {
+  }
+
+  @Module
+  @InstallIn(ViewModelComponent.class)
+  public abstract static class BindsModule {
+    private BindsModule() {
+    }
+
+    @Binds
+    @IntoMap
+    @LazyClassKey(NewGroupViewModel.class)
+    @HiltViewModelMap
+    public abstract ViewModel binds(NewGroupViewModel vm);
+  }
+
+  @Module
+  @InstallIn(ActivityRetainedComponent.class)
+  public static final class KeyModule {
+    private KeyModule() {
+    }
+
+    @Provides
+    @IntoMap
+    @LazyClassKey(NewGroupViewModel.class)
+    @HiltViewModelMap.KeySet
+    public static boolean provide() {
+      return true;
+    }
+  }
+}
