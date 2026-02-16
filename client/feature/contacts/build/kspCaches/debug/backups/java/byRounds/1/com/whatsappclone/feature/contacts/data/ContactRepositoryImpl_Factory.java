b62758cfc6cd176a75f48188a026a6d631a2f188
Tarun Gusain <Tarun.Gusain@gruve.ai@Taruns-MacBook-Pro.local>
2026-02-16 11:32:17 +0530

    vibe coded the client and all the backend services: ContactRepositoryImpl_Factory.java

---
+++ client/feature/contacts/build/kspCaches/debug/backups/java/byRounds/1/com/whatsappclone/feature/contacts/data/ContactRepositoryImpl_Factory.java
@@ -0,0 +1,63 @@
+package com.whatsappclone.feature.contacts.data;
+
+import android.content.ContentResolver;
+import com.whatsappclone.core.database.dao.ContactDao;
+import com.whatsappclone.core.database.dao.UserDao;
+import com.whatsappclone.core.network.api.UserApi;
+import dagger.internal.DaggerGenerated;
+import dagger.internal.Factory;
+import dagger.internal.QualifierMetadata;
+import dagger.internal.ScopeMetadata;
+import javax.annotation.processing.Generated;
+import javax.inject.Provider;
+
+@ScopeMetadata("javax.inject.Singleton")
+@QualifierMetadata
+@DaggerGenerated
+@Generated(
+    value = "dagger.internal.codegen.ComponentProcessor",
+    comments = "https://dagger.dev"
+)
+@SuppressWarnings({
+    "unchecked",
+    "rawtypes",
+    "KotlinInternal",
+    "KotlinInternalInJava",
+    "cast",
+    "deprecation",
+    "nullness:initialization.field.uninitialized"
+})
+public final class ContactRepositoryImpl_Factory implements Factory<ContactRepositoryImpl> {
+  private final Provider<ContentResolver> contentResolverProvider;
+
+  private final Provider<UserApi> userApiProvider;
+
+  private final Provider<ContactDao> contactDaoProvider;
+
+  private final Provider<UserDao> userDaoProvider;
+
+  public ContactRepositoryImpl_Factory(Provider<ContentResolver> contentResolverProvider,
+      Provider<UserApi> userApiProvider, Provider<ContactDao> contactDaoProvider,
+      Provider<UserDao> userDaoProvider) {
+    this.contentResolverProvider = contentResolverProvider;
+    this.userApiProvider = userApiProvider;
+    this.contactDaoProvider = contactDaoProvider;
+    this.userDaoProvider = userDaoProvider;
+  }
+
+  @Override
+  public ContactRepositoryImpl get() {
+    return newInstance(contentResolverProvider.get(), userApiProvider.get(), contactDaoProvider.get(), userDaoProvider.get());
+  }
+
+  public static ContactRepositoryImpl_Factory create(
+      Provider<ContentResolver> contentResolverProvider, Provider<UserApi> userApiProvider,
+      Provider<ContactDao> contactDaoProvider, Provider<UserDao> userDaoProvider) {
+    return new ContactRepositoryImpl_Factory(contentResolverProvider, userApiProvider, contactDaoProvider, userDaoProvider);
+  }
+
+  public static ContactRepositoryImpl newInstance(ContentResolver contentResolver, UserApi userApi,
+      ContactDao contactDao, UserDao userDao) {
+    return new ContactRepositoryImpl(contentResolver, userApi, contactDao, userDao);
+  }
+}
