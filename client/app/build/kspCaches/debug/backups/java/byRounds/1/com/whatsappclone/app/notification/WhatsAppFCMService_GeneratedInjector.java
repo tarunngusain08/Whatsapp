7731d9d3657433aaf1b98917075faf99763a1684
Tarun Gusain <Tarun.Gusain@gruve.ai@Taruns-MacBook-Pro.local>
2026-02-16 11:25:55 +0530

    vibe coded the client and all the backend services: WhatsAppFCMService_GeneratedInjector.java

---
+++ client/app/build/kspCaches/debug/backups/java/byRounds/1/com/whatsappclone/app/notification/WhatsAppFCMService_GeneratedInjector.java
@@ -0,0 +1,17 @@
+package com.whatsappclone.app.notification;
+
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.ServiceComponent;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.hilt.internal.GeneratedEntryPoint;
+import javax.annotation.processing.Generated;
+
+@OriginatingElement(
+    topLevelClass = WhatsAppFCMService.class
+)
+@GeneratedEntryPoint
+@InstallIn(ServiceComponent.class)
+@Generated("dagger.hilt.android.processor.internal.androidentrypoint.InjectorEntryPointGenerator")
+public interface WhatsAppFCMService_GeneratedInjector {
+  void injectWhatsAppFCMService(WhatsAppFCMService whatsAppFCMService);
+}
