b2ad9d3f5083f314b0769797e83f3533879f7df6
Tarun Gusain <Tarun.Gusain@gruve.ai@Taruns-MacBook-Pro.local>
2026-02-16 11:27:31 +0530

    vibe coded the client and all the backend services: NetworkModule_ProvideMessageApiFactory.java

---
+++ client/core/network/build/generated/ksp/debug/java/com/whatsappclone/core/network/di/NetworkModule_ProvideMessageApiFactory.java
@@ -0,0 +1,48 @@
+package com.whatsappclone.core.network.di;
+
+import com.whatsappclone.core.network.api.MessageApi;
+import dagger.internal.DaggerGenerated;
+import dagger.internal.Factory;
+import dagger.internal.Preconditions;
+import dagger.internal.QualifierMetadata;
+import dagger.internal.ScopeMetadata;
+import javax.annotation.processing.Generated;
+import javax.inject.Provider;
+import retrofit2.Retrofit;
+
+@ScopeMetadata("javax.inject.Singleton")
+@QualifierMetadata
+@DaggerGenerated
+@Generated(
+    value = "dagger.internal.codegen.ComponentProcessor",
+    comments = "https://dagger.dev"
+)
+@SuppressWarnings({
+    "unchecked",
+    "rawtypes",
+    "KotlinInternal",
+    "KotlinInternalInJava",
+    "cast",
+    "deprecation",
+    "nullness:initialization.field.uninitialized"
+})
+public final class NetworkModule_ProvideMessageApiFactory implements Factory<MessageApi> {
+  private final Provider<Retrofit> retrofitProvider;
+
+  public NetworkModule_ProvideMessageApiFactory(Provider<Retrofit> retrofitProvider) {
+    this.retrofitProvider = retrofitProvider;
+  }
+
+  @Override
+  public MessageApi get() {
+    return provideMessageApi(retrofitProvider.get());
+  }
+
+  public static NetworkModule_ProvideMessageApiFactory create(Provider<Retrofit> retrofitProvider) {
+    return new NetworkModule_ProvideMessageApiFactory(retrofitProvider);
+  }
+
+  public static MessageApi provideMessageApi(Retrofit retrofit) {
+    return Preconditions.checkNotNullFromProvides(NetworkModule.INSTANCE.provideMessageApi(retrofit));
+  }
+}
