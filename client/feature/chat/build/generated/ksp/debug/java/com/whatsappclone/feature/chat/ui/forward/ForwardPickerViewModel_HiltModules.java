d636862892a502e20d81f9d9b41109e246615f31
Tarun Gusain <Tarun.Gusain@gruve.ai@Taruns-MacBook-Pro.local>
2026-02-16 11:30:03 +0530

    vibe coded the client and all the backend services: ForwardPickerViewModel_HiltModules.java

---
+++ client/feature/chat/build/generated/ksp/debug/java/com/whatsappclone/feature/chat/ui/forward/ForwardPickerViewModel_HiltModules.java
@@ -0,0 +1,51 @@
+package com.whatsappclone.feature.chat.ui.forward;
+
+import androidx.lifecycle.ViewModel;
+import dagger.Binds;
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.ActivityRetainedComponent;
+import dagger.hilt.android.components.ViewModelComponent;
+import dagger.hilt.android.internal.lifecycle.HiltViewModelMap;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.multibindings.IntoMap;
+import dagger.multibindings.LazyClassKey;
+import javax.annotation.processing.Generated;
+
+@Generated("dagger.hilt.android.processor.internal.viewmodel.ViewModelProcessor")
+@OriginatingElement(
+    topLevelClass = ForwardPickerViewModel.class
+)
+public final class ForwardPickerViewModel_HiltModules {
+  private ForwardPickerViewModel_HiltModules() {
+  }
+
+  @Module
+  @InstallIn(ViewModelComponent.class)
+  public abstract static class BindsModule {
+    private BindsModule() {
+    }
+
+    @Binds
+    @IntoMap
+    @LazyClassKey(ForwardPickerViewModel.class)
+    @HiltViewModelMap
+    public abstract ViewModel binds(ForwardPickerViewModel vm);
+  }
+
+  @Module
+  @InstallIn(ActivityRetainedComponent.class)
+  public static final class KeyModule {
+    private KeyModule() {
+    }
+
+    @Provides
+    @IntoMap
+    @LazyClassKey(ForwardPickerViewModel.class)
+    @HiltViewModelMap.KeySet
+    public static boolean provide() {
+      return true;
+    }
+  }
+}
