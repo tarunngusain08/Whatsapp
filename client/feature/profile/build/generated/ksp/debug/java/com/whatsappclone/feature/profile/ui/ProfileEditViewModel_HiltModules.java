401a55e98db332ac15eec174a62205b349344241
Tarun Gusain <Tarun.Gusain@gruve.ai@Taruns-MacBook-Pro.local>
2026-02-16 11:34:38 +0530

    vibe coded the client and all the backend services: ProfileEditViewModel_HiltModules.java

---
+++ client/feature/profile/build/generated/ksp/debug/java/com/whatsappclone/feature/profile/ui/ProfileEditViewModel_HiltModules.java
@@ -0,0 +1,51 @@
+package com.whatsappclone.feature.profile.ui;
+
+import androidx.lifecycle.ViewModel;
+import dagger.Binds;
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.ActivityRetainedComponent;
+import dagger.hilt.android.components.ViewModelComponent;
+import dagger.hilt.android.internal.lifecycle.HiltViewModelMap;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.multibindings.IntoMap;
+import dagger.multibindings.LazyClassKey;
+import javax.annotation.processing.Generated;
+
+@Generated("dagger.hilt.android.processor.internal.viewmodel.ViewModelProcessor")
+@OriginatingElement(
+    topLevelClass = ProfileEditViewModel.class
+)
+public final class ProfileEditViewModel_HiltModules {
+  private ProfileEditViewModel_HiltModules() {
+  }
+
+  @Module
+  @InstallIn(ViewModelComponent.class)
+  public abstract static class BindsModule {
+    private BindsModule() {
+    }
+
+    @Binds
+    @IntoMap
+    @LazyClassKey(ProfileEditViewModel.class)
+    @HiltViewModelMap
+    public abstract ViewModel binds(ProfileEditViewModel vm);
+  }
+
+  @Module
+  @InstallIn(ActivityRetainedComponent.class)
+  public static final class KeyModule {
+    private KeyModule() {
+    }
+
+    @Provides
+    @IntoMap
+    @LazyClassKey(ProfileEditViewModel.class)
+    @HiltViewModelMap.KeySet
+    public static boolean provide() {
+      return true;
+    }
+  }
+}
