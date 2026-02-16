bed29a8bd49a6831870c034f9a033af8e7f2fa0f
Tarun Gusain <Tarun.Gusain@gruve.ai@Taruns-MacBook-Pro.local>
2026-02-16 11:33:50 +0530

    vibe coded the client and all the backend services: MediaViewerViewModel_HiltModules.java

---
+++ client/feature/media/build/generated/ksp/debug/java/com/whatsappclone/feature/media/ui/MediaViewerViewModel_HiltModules.java
@@ -0,0 +1,51 @@
+package com.whatsappclone.feature.media.ui;
+
+import androidx.lifecycle.ViewModel;
+import dagger.Binds;
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.ActivityRetainedComponent;
+import dagger.hilt.android.components.ViewModelComponent;
+import dagger.hilt.android.internal.lifecycle.HiltViewModelMap;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.multibindings.IntoMap;
+import dagger.multibindings.LazyClassKey;
+import javax.annotation.processing.Generated;
+
+@Generated("dagger.hilt.android.processor.internal.viewmodel.ViewModelProcessor")
+@OriginatingElement(
+    topLevelClass = MediaViewerViewModel.class
+)
+public final class MediaViewerViewModel_HiltModules {
+  private MediaViewerViewModel_HiltModules() {
+  }
+
+  @Module
+  @InstallIn(ViewModelComponent.class)
+  public abstract static class BindsModule {
+    private BindsModule() {
+    }
+
+    @Binds
+    @IntoMap
+    @LazyClassKey(MediaViewerViewModel.class)
+    @HiltViewModelMap
+    public abstract ViewModel binds(MediaViewerViewModel vm);
+  }
+
+  @Module
+  @InstallIn(ActivityRetainedComponent.class)
+  public static final class KeyModule {
+    private KeyModule() {
+    }
+
+    @Provides
+    @IntoMap
+    @LazyClassKey(MediaViewerViewModel.class)
+    @HiltViewModelMap.KeySet
+    public static boolean provide() {
+      return true;
+    }
+  }
+}
