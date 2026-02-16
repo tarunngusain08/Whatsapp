d398301b0446fec88dc8aa8ff72ee8d21498876a
Tarun Gusain <Tarun.Gusain@gruve.ai@Taruns-MacBook-Pro.local>
2026-02-16 11:32:44 +0530

    vibe coded the client and all the backend services: GroupInfoViewModel_HiltModules_BindsModule_Binds_LazyMapKey.java

---
+++ client/feature/group/build/generated/ksp/debug/java/com/whatsappclone/feature/group/ui/info/GroupInfoViewModel_HiltModules_BindsModule_Binds_LazyMapKey.java
@@ -0,0 +1,28 @@
+package com.whatsappclone.feature.group.ui.info;
+
+import dagger.internal.DaggerGenerated;
+import dagger.internal.IdentifierNameString;
+import dagger.internal.KeepFieldType;
+import javax.annotation.processing.Generated;
+
+@IdentifierNameString
+@DaggerGenerated
+@Generated(
+    value = "dagger.internal.codegen.ComponentProcessor",
+    comments = "https://dagger.dev"
+)
+@SuppressWarnings({
+    "unchecked",
+    "rawtypes",
+    "KotlinInternal",
+    "KotlinInternalInJava",
+    "cast",
+    "deprecation",
+    "nullness:initialization.field.uninitialized"
+})
+public final class GroupInfoViewModel_HiltModules_BindsModule_Binds_LazyMapKey {
+  @KeepFieldType
+  static GroupInfoViewModel keepFieldType;
+
+  public static String lazyClassKeyName = "com.whatsappclone.feature.group.ui.info.GroupInfoViewModel";
+}
