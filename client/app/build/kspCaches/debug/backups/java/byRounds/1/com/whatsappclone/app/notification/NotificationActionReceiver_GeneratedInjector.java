f3534b05873b0a790d3dfbc6c3a247dd21002233
Tarun Gusain <Tarun.Gusain@gruve.ai@Taruns-MacBook-Pro.local>
2026-02-16 11:25:55 +0530

    vibe coded the client and all the backend services: NotificationActionReceiver_GeneratedInjector.java

---
+++ client/app/build/kspCaches/debug/backups/java/byRounds/1/com/whatsappclone/app/notification/NotificationActionReceiver_GeneratedInjector.java
@@ -0,0 +1,17 @@
+package com.whatsappclone.app.notification;
+
+import dagger.hilt.InstallIn;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.hilt.components.SingletonComponent;
+import dagger.hilt.internal.GeneratedEntryPoint;
+import javax.annotation.processing.Generated;
+
+@OriginatingElement(
+    topLevelClass = NotificationActionReceiver.class
+)
+@GeneratedEntryPoint
+@InstallIn(SingletonComponent.class)
+@Generated("dagger.hilt.android.processor.internal.androidentrypoint.InjectorEntryPointGenerator")
+public interface NotificationActionReceiver_GeneratedInjector {
+  void injectNotificationActionReceiver(NotificationActionReceiver notificationActionReceiver);
+}
