66357721db376f68856fbed5457fa89a2b574b0c
Tarun Gusain <Tarun.Gusain@gruve.ai@Taruns-MacBook-Pro.local>
2026-02-16 11:34:38 +0530

    vibe coded the client and all the backend services: _com_whatsappclone_feature_profile_di_ProfileModule.java

---
+++ client/feature/profile/build/generated/ksp/debug/java/hilt_aggregated_deps/_com_whatsappclone_feature_profile_di_ProfileModule.java
@@ -0,0 +1,15 @@
+package hilt_aggregated_deps;
+
+import dagger.hilt.processor.internal.aggregateddeps.AggregatedDeps;
+import javax.annotation.processing.Generated;
+
+/**
+ * This class should only be referenced by generated code! This class aggregates information across multiple compilations.
+ */
+@AggregatedDeps(
+    components = "dagger.hilt.components.SingletonComponent",
+    modules = "com.whatsappclone.feature.profile.di.ProfileModule"
+)
+@Generated("dagger.hilt.processor.internal.aggregateddeps.AggregatedDepsGenerator")
+public class _com_whatsappclone_feature_profile_di_ProfileModule {
+}
