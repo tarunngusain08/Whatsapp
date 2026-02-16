f7a125f23a4c9bde06d1c99f2c53131b6eb497f2
Tarun Gusain <Tarun.Gusain@gruve.ai@Taruns-MacBook-Pro.local>
2026-02-16 11:30:03 +0530

    vibe coded the client and all the backend services: ChatDetailViewModel_HiltModules_KeyModule_Provide_LazyMapKey.java

---
+++ client/feature/chat/build/generated/ksp/debug/java/com/whatsappclone/feature/chat/ui/chatdetail/ChatDetailViewModel_HiltModules_KeyModule_Provide_LazyMapKey.java
@@ -0,0 +1,28 @@
+package com.whatsappclone.feature.chat.ui.chatdetail;
+
+import dagger.internal.DaggerGenerated;
+import dagger.internal.IdentifierNameString;
+import dagger.internal.KeepFieldType;
+import javax.annotation.processing.Generated;
+
+@IdentifierNameString
+@DaggerGenerated
+@Generated(
+    value = "dagger.internal.codegen.ComponentProcessor",
+    comments = "https://dagger.dev"
+)
+@SuppressWarnings({
+    "unchecked",
+    "rawtypes",
+    "KotlinInternal",
+    "KotlinInternalInJava",
+    "cast",
+    "deprecation",
+    "nullness:initialization.field.uninitialized"
+})
+public final class ChatDetailViewModel_HiltModules_KeyModule_Provide_LazyMapKey {
+  @KeepFieldType
+  static ChatDetailViewModel keepFieldType;
+
+  public static String lazyClassKeyName = "com.whatsappclone.feature.chat.ui.chatdetail.ChatDetailViewModel";
+}
