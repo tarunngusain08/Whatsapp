740c1cfa19f618a6a92c74be994f3fa08dab49b5
Tarun Gusain <Tarun.Gusain@gruve.ai@Taruns-MacBook-Pro.local>
2026-02-16 11:25:54 +0530

    vibe coded the client and all the backend services: WhatsAppApplication_GeneratedInjector.java

---
+++ client/app/build/kspCaches/debug/backups/java/byRounds/1/com/whatsappclone/app/WhatsAppApplication_GeneratedInjector.java
@@ -0,0 +1,17 @@
+package com.whatsappclone.app;
+
+import dagger.hilt.InstallIn;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.hilt.components.SingletonComponent;
+import dagger.hilt.internal.GeneratedEntryPoint;
+import javax.annotation.processing.Generated;
+
+@OriginatingElement(
+    topLevelClass = WhatsAppApplication.class
+)
+@GeneratedEntryPoint
+@InstallIn(SingletonComponent.class)
+@Generated("dagger.hilt.android.processor.internal.androidentrypoint.InjectorEntryPointGenerator")
+public interface WhatsAppApplication_GeneratedInjector {
+  void injectWhatsAppApplication(WhatsAppApplication whatsAppApplication);
+}
