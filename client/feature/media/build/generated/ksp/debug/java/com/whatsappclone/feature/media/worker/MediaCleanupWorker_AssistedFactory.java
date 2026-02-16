4d20110fe5e427cda8945d75e0c2a27bd0696222
Tarun Gusain <Tarun.Gusain@gruve.ai@Taruns-MacBook-Pro.local>
2026-02-16 11:33:51 +0530

    vibe coded the client and all the backend services: MediaCleanupWorker_AssistedFactory.java

---
+++ client/feature/media/build/generated/ksp/debug/java/com/whatsappclone/feature/media/worker/MediaCleanupWorker_AssistedFactory.java
@@ -0,0 +1,10 @@
+package com.whatsappclone.feature.media.worker;
+
+import androidx.hilt.work.WorkerAssistedFactory;
+import dagger.assisted.AssistedFactory;
+import javax.annotation.processing.Generated;
+
+@Generated("androidx.hilt.AndroidXHiltProcessor")
+@AssistedFactory
+public interface MediaCleanupWorker_AssistedFactory extends WorkerAssistedFactory<MediaCleanupWorker> {
+}
