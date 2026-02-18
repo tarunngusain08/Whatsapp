9c386460448ba564316d9dc8efc5f0c1a0698c1b
Tarun Gusain <Tarun.Gusain@gruve.ai@Taruns-MacBook-Pro.local>
2026-02-18 16:05:12 +0530

    fixing crashing issues: SettingsViewModel_Factory.java

--- client/feature/settings/build/generated/ksp/debug/java/com/whatsappclone/feature/settings/ui/SettingsViewModel_Factory.java
+++ client/feature/settings/build/generated/ksp/debug/java/com/whatsappclone/feature/settings/ui/SettingsViewModel_Factory.java
@@ -2,6 +2,7 @@ package com.whatsappclone.feature.settings.ui;
 
 import android.content.SharedPreferences;
 import com.whatsappclone.core.database.dao.UserDao;
+import com.whatsappclone.core.network.api.UserApi;
 import com.whatsappclone.feature.auth.data.AuthRepository;
 import dagger.internal.DaggerGenerated;
 import dagger.internal.Factory;
@@ -31,27 +32,32 @@ public final class SettingsViewModel_Factory implements Factory<SettingsViewMode
 
   private final Provider<UserDao> userDaoProvider;
 
+  private final Provider<UserApi> userApiProvider;
+
   private final Provider<SharedPreferences> encryptedPrefsProvider;
 
   public SettingsViewModel_Factory(Provider<AuthRepository> authRepositoryProvider,
-      Provider<UserDao> userDaoProvider, Provider<SharedPreferences> encryptedPrefsProvider) {
+      Provider<UserDao> userDaoProvider, Provider<UserApi> userApiProvider,
+      Provider<SharedPreferences> encryptedPrefsProvider) {
     this.authRepositoryProvider = authRepositoryProvider;
     this.userDaoProvider = userDaoProvider;
+    this.userApiProvider = userApiProvider;
     this.encryptedPrefsProvider = encryptedPrefsProvider;
   }
 
   @Override
   public SettingsViewModel get() {
-    return newInstance(authRepositoryProvider.get(), userDaoProvider.get(), encryptedPrefsProvider.get());
+    return newInstance(authRepositoryProvider.get(), userDaoProvider.get(), userApiProvider.get(), encryptedPrefsProvider.get());
   }
 
   public static SettingsViewModel_Factory create(Provider<AuthRepository> authRepositoryProvider,
-      Provider<UserDao> userDaoProvider, Provider<SharedPreferences> encryptedPrefsProvider) {
-    return new SettingsViewModel_Factory(authRepositoryProvider, userDaoProvider, encryptedPrefsProvider);
+      Provider<UserDao> userDaoProvider, Provider<UserApi> userApiProvider,
+      Provider<SharedPreferences> encryptedPrefsProvider) {
+    return new SettingsViewModel_Factory(authRepositoryProvider, userDaoProvider, userApiProvider, encryptedPrefsProvider);
   }
 
   public static SettingsViewModel newInstance(AuthRepository authRepository, UserDao userDao,
-      SharedPreferences encryptedPrefs) {
-    return new SettingsViewModel(authRepository, userDao, encryptedPrefs);
+      UserApi userApi, SharedPreferences encryptedPrefs) {
+    return new SettingsViewModel(authRepository, userDao, userApi, encryptedPrefs);
   }
 }
