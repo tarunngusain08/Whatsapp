c12c69c1623bab8d5f0974fa98b44900bf53faab
Tarun Gusain <Tarun.Gusain@gruve.ai@Taruns-MacBook-Pro.local>
2026-02-18 16:03:49 +0530

    fixing crashing issues: WhatsAppApplication_MembersInjector.java

--- client/app/build/kspCaches/debug/backups/java/byRounds/1/com/whatsappclone/app/WhatsAppApplication_MembersInjector.java
+++ client/app/build/kspCaches/debug/backups/java/byRounds/1/com/whatsappclone/app/WhatsAppApplication_MembersInjector.java
@@ -1,6 +1,7 @@
 package com.whatsappclone.app;
 
 import androidx.hilt.work.HiltWorkerFactory;
+import coil3.ImageLoader;
 import dagger.MembersInjector;
 import dagger.internal.DaggerGenerated;
 import dagger.internal.InjectedFieldSignature;
@@ -26,23 +27,34 @@ import javax.inject.Provider;
 public final class WhatsAppApplication_MembersInjector implements MembersInjector<WhatsAppApplication> {
   private final Provider<HiltWorkerFactory> workerFactoryProvider;
 
-  public WhatsAppApplication_MembersInjector(Provider<HiltWorkerFactory> workerFactoryProvider) {
+  private final Provider<ImageLoader> imageLoaderProvider;
+
+  public WhatsAppApplication_MembersInjector(Provider<HiltWorkerFactory> workerFactoryProvider,
+      Provider<ImageLoader> imageLoaderProvider) {
     this.workerFactoryProvider = workerFactoryProvider;
+    this.imageLoaderProvider = imageLoaderProvider;
   }
 
   public static MembersInjector<WhatsAppApplication> create(
-      Provider<HiltWorkerFactory> workerFactoryProvider) {
-    return new WhatsAppApplication_MembersInjector(workerFactoryProvider);
+      Provider<HiltWorkerFactory> workerFactoryProvider,
+      Provider<ImageLoader> imageLoaderProvider) {
+    return new WhatsAppApplication_MembersInjector(workerFactoryProvider, imageLoaderProvider);
   }
 
   @Override
   public void injectMembers(WhatsAppApplication instance) {
     injectWorkerFactory(instance, workerFactoryProvider.get());
+    injectImageLoader(instance, imageLoaderProvider.get());
   }
 
   @InjectedFieldSignature("com.whatsappclone.app.WhatsAppApplication.workerFactory")
   public static void injectWorkerFactory(WhatsAppApplication instance,
       HiltWorkerFactory workerFactory) {
     instance.workerFactory = workerFactory;
+  }
+
+  @InjectedFieldSignature("com.whatsappclone.app.WhatsAppApplication.imageLoader")
+  public static void injectImageLoader(WhatsAppApplication instance, ImageLoader imageLoader) {
+    instance.imageLoader = imageLoader;
   }
 }
