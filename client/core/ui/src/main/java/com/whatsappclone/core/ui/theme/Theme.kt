5f2e986c1a78a6fbc96abd7372204552b7f2d914
Tarun Gusain <Tarun.Gusain@gruve.ai@Taruns-MacBook-Pro.local>
2026-02-19 16:56:18 +0530

    stablising the app and introducing new features: Theme.kt

--- client/core/ui/src/main/java/com/whatsappclone/core/ui/theme/Theme.kt
+++ client/core/ui/src/main/java/com/whatsappclone/core/ui/theme/Theme.kt
@@ -78,19 +78,61 @@ private val DarkColorScheme = darkColorScheme(
 data class WhatsAppColorScheme(
     val sentBubble: Color,
     val receivedBubble: Color,
-    val chatBackground: Color
+    val chatBackground: Color,
+    val messageMeta: Color,
+    val deletedText: Color,
+    val forwardedText: Color,
+    val starColor: Color,
+    val systemBubble: Color,
+    val systemText: Color,
+    val typingGreen: Color,
+    val onlineGreen: Color,
+    val messageHighlight: Color,
+    val selectionHighlight: Color,
+    val mutedIcon: Color,
+    val pinnedBackground: Color,
+    val durationBadge: Color,
+    val downloadOverlay: Color
 )
 
 private val LightWhatsAppColors = WhatsAppColorScheme(
     sentBubble = LightSentBubble,
     receivedBubble = LightReceivedBubble,
-    chatBackground = LightChatBackground
+    chatBackground = LightChatBackground,
+    messageMeta = Color(0xFF667781),
+    deletedText = Color(0xFF8696A0),
+    forwardedText = Color(0xFF8696A0),
+    starColor = Color(0xFFFFC107),
+    systemBubble = Color(0xFFE2F0FD),
+    systemText = Color(0xFF54656F),
+    typingGreen = Color(0xFF25D366),
+    onlineGreen = Color(0xFF25D366),
+    messageHighlight = Color(0x3300A884),
+    selectionHighlight = Color(0x2200A884),
+    mutedIcon = Color(0xFFBDBDBD),
+    pinnedBackground = Color(0xFFF0F4F0),
+    durationBadge = Color(0xCC000000),
+    downloadOverlay = Color(0x80000000)
 )
 
 private val DarkWhatsAppColors = WhatsAppColorScheme(
     sentBubble = DarkSentBubble,
     receivedBubble = DarkReceivedBubble,
-    chatBackground = DarkChatBackground
+    chatBackground = DarkChatBackground,
+    messageMeta = Color(0xFF8696A0),
+    deletedText = Color(0xFF8696A0),
+    forwardedText = Color(0xFF8696A0),
+    starColor = Color(0xFFFFC107),
+    systemBubble = Color(0xFF182229),
+    systemText = Color(0xFF8696A0),
+    typingGreen = Color(0xFF25D366),
+    onlineGreen = Color(0xFF25D366),
+    messageHighlight = Color(0x3300A884),
+    selectionHighlight = Color(0x2200A884),
+    mutedIcon = Color(0xFF6B7B85),
+    pinnedBackground = Color(0xFF1F2C34),
+    durationBadge = Color(0xCC000000),
+    downloadOverlay = Color(0x80000000)
 )
 
 val LocalWhatsAppColors = staticCompositionLocalOf {
@@ -102,17 +144,23 @@ object WhatsAppColors {
         @Composable
         get() = LocalWhatsAppColors.current
 
-    val SentBubble: Color
-        @Composable
-        get() = current.sentBubble
-
-    val ReceivedBubble: Color
-        @Composable
-        get() = current.receivedBubble
-
-    val ChatBackground: Color
-        @Composable
-        get() = current.chatBackground
+    val SentBubble: Color @Composable get() = current.sentBubble
+    val ReceivedBubble: Color @Composable get() = current.receivedBubble
+    val ChatBackground: Color @Composable get() = current.chatBackground
+    val MessageMeta: Color @Composable get() = current.messageMeta
+    val DeletedText: Color @Composable get() = current.deletedText
+    val ForwardedText: Color @Composable get() = current.forwardedText
+    val StarColor: Color @Composable get() = current.starColor
+    val SystemBubble: Color @Composable get() = current.systemBubble
+    val SystemText: Color @Composable get() = current.systemText
+    val TypingGreen: Color @Composable get() = current.typingGreen
+    val OnlineGreen: Color @Composable get() = current.onlineGreen
+    val MessageHighlight: Color @Composable get() = current.messageHighlight
+    val SelectionHighlight: Color @Composable get() = current.selectionHighlight
+    val MutedIcon: Color @Composable get() = current.mutedIcon
+    val PinnedBackground: Color @Composable get() = current.pinnedBackground
+    val DurationBadge: Color @Composable get() = current.durationBadge
+    val DownloadOverlay: Color @Composable get() = current.downloadOverlay
 }
 
 @Composable
