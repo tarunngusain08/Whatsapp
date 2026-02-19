fc1d2634db7e5ddc101f1df429cf3061350fb98d
tarunngusain08 <prudent.tarun0808@gmail.com>
2026-02-19 20:12:40 +0530

    fixing issues: NewGroupViewModel_Factory.java

--- client/feature/group/build/kspCaches/debug/backups/java/byRounds/1/com/whatsappclone/feature/group/ui/newgroup/NewGroupViewModel_Factory.java
+++ client/feature/group/build/kspCaches/debug/backups/java/byRounds/1/com/whatsappclone/feature/group/ui/newgroup/NewGroupViewModel_Factory.java
@@ -1,7 +1,9 @@
 package com.whatsappclone.feature.group.ui.newgroup;
 
 import com.whatsappclone.core.database.dao.UserDao;
+import com.whatsappclone.core.network.url.BaseUrlProvider;
 import com.whatsappclone.feature.group.domain.CreateGroupUseCase;
+import com.whatsappclone.feature.media.data.MediaRepository;
 import dagger.internal.DaggerGenerated;
 import dagger.internal.Factory;
 import dagger.internal.QualifierMetadata;
@@ -30,24 +32,35 @@ public final class NewGroupViewModel_Factory implements Factory<NewGroupViewMode
 
   private final Provider<CreateGroupUseCase> createGroupUseCaseProvider;
 
+  private final Provider<MediaRepository> mediaRepositoryProvider;
+
+  private final Provider<BaseUrlProvider> baseUrlProvider;
+
   public NewGroupViewModel_Factory(Provider<UserDao> userDaoProvider,
-      Provider<CreateGroupUseCase> createGroupUseCaseProvider) {
+      Provider<CreateGroupUseCase> createGroupUseCaseProvider,
+      Provider<MediaRepository> mediaRepositoryProvider,
+      Provider<BaseUrlProvider> baseUrlProvider) {
     this.userDaoProvider = userDaoProvider;
     this.createGroupUseCaseProvider = createGroupUseCaseProvider;
+    this.mediaRepositoryProvider = mediaRepositoryProvider;
+    this.baseUrlProvider = baseUrlProvider;
   }
 
   @Override
   public NewGroupViewModel get() {
-    return newInstance(userDaoProvider.get(), createGroupUseCaseProvider.get());
+    return newInstance(userDaoProvider.get(), createGroupUseCaseProvider.get(), mediaRepositoryProvider.get(), baseUrlProvider.get());
   }
 
   public static NewGroupViewModel_Factory create(Provider<UserDao> userDaoProvider,
-      Provider<CreateGroupUseCase> createGroupUseCaseProvider) {
-    return new NewGroupViewModel_Factory(userDaoProvider, createGroupUseCaseProvider);
+      Provider<CreateGroupUseCase> createGroupUseCaseProvider,
+      Provider<MediaRepository> mediaRepositoryProvider,
+      Provider<BaseUrlProvider> baseUrlProvider) {
+    return new NewGroupViewModel_Factory(userDaoProvider, createGroupUseCaseProvider, mediaRepositoryProvider, baseUrlProvider);
   }
 
   public static NewGroupViewModel newInstance(UserDao userDao,
-      CreateGroupUseCase createGroupUseCase) {
-    return new NewGroupViewModel(userDao, createGroupUseCase);
+      CreateGroupUseCase createGroupUseCase, MediaRepository mediaRepository,
+      BaseUrlProvider baseUrlProvider) {
+    return new NewGroupViewModel(userDao, createGroupUseCase, mediaRepository, baseUrlProvider);
   }
 }
