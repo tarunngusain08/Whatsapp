c21ef8b90a7c8c4fead51a6a1d75aef73c9c02e0
tarunngusain08 <prudent.tarun0808@gmail.com>
2026-02-20 06:40:09 +0530

    fixing issues: WhatsAppApplication.kt

--- client/app/src/main/java/com/whatsappclone/app/WhatsAppApplication.kt
+++ client/app/src/main/java/com/whatsappclone/app/WhatsAppApplication.kt
@@ -11,6 +11,8 @@ import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import coil3.ImageLoader
 import coil3.SingletonImageLoader
+import com.whatsappclone.core.common.util.UrlResolver
+import com.whatsappclone.core.network.url.BaseUrlProvider
 import com.whatsappclone.feature.chat.worker.ScheduledMessageWorker
 import dagger.hilt.android.HiltAndroidApp
 import java.util.concurrent.TimeUnit
@@ -34,6 +36,7 @@ class WhatsAppApplication : Application(), Configuration.Provider, SingletonImag
 
     override fun onCreate() {
         super.onCreate()
+        UrlResolver.init(BaseUrlProvider.DEFAULT_BASE_URL)
         createNotificationChannels()
         enqueueScheduledMessageWorker()
     }
