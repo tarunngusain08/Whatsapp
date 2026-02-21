2fefc242a7671e3a303f0ee597a4ec2b14fac581
tarunngusain08 <prudent.tarun0808@gmail.com>
2026-02-21 08:26:03 +0530

    Finding bugs and resolving them: TypingStateHolder.kt

--- client/core/network/src/main/java/com/whatsappclone/core/network/websocket/TypingStateHolder.kt
+++ client/core/network/src/main/java/com/whatsappclone/core/network/websocket/TypingStateHolder.kt
@@ -11,6 +11,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.util.concurrent.ConcurrentHashMap
 import javax.inject.Inject
